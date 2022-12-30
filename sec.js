7502fc9cefe6aab28639dd2dfcb476c7bd44c1c6
R7558 <rupsanaworld1004@gmail.co>
2022-12-30 20:45:40 +0600

    added all

---
+++ sec.js
@@ -0,0 +1,10 @@
+var eggPrice=10;
+var eggQuantity=20;
+var potatoPrice=30;
+var potatoQuantity=3;
+var eggTotal=eggPrice*eggQuantity;
+var potatoTotal=potatoPrice*potatoQuantity;
+var totalPrice=eggTotal+potatoTotal;
+console.log(eggTotal);
+console.log(potatoTotal);
+console.log(totalPrice);
